693548c8d54fd1f251cbb603951ccfd30e504a96
spen940408@gmail.com <spen940408@gmail.com>
2024-11-22 15:39:31 +0800

    controy.py bug fixed./ move to Server

--- Server/control.py
+++ Server/control.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env pybricks-micropython
 from pybricks.hubs import EV3Brick
-from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
-                                 InfraredSensor, UltrasonicSensor, GyroSensor)
-from pybricks.parameters import Port, Stop, Direction, Button, Color
-from pybricks.tools import wait, StopWatch, DataLog
-from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
-
-# import package that can calculate arctan
-from math import atan2, pi
-
+from pybricks.ev3devices import Motor
+from pybricks.parameters import Port, Stop
+from pybricks.tools import wait
+from math import atan2
 
 ev3 = EV3Brick()
 
@@ -19,40 +13,37 @@ class Robot_Car:
         self.motor_right = Motor(Port.B)
         self.speed = 200 
         self.location = [0, 0]
-        self.orientation = 0 # [0,2pi]
+        self.orientation = 0  # [0, 2π]
+
     def relocate(self, x, y):
         self.location = [x, y]
-    
+
     def move_forward(self, length):
-        # distance per second = 0.3 * speed
         self.motor_left.run(self.speed)
         self.motor_right.run(self.speed)
         duration = length / (0.3 * self.speed)
-        wait(duration)
+        wait(duration * 1000)  # Convert to ms
         self.stop()
 
     def move_backward(self, length):
-        # distance per second = 0.3 * speed
         self.motor_left.run(-self.speed)
         self.motor_right.run(-self.speed)
         duration = length / (0.3 * self.speed)
-        wait(duration)
+        wait(duration * 1000)  # Convert to ms
         self.stop()
 
     def turn_left(self, angle):
-        # angle per second = 0.47 * speed
         self.motor_left.run(-self.speed)
         self.motor_right.run(self.speed)
         duration = angle / (0.47 * self.speed)
-        wait(duration)
+        wait(duration * 1000)  # Convert to ms
         self.stop()
 
     def turn_right(self, angle):
-        # angle per second = 0.47 * speed
         self.motor_left.run(self.speed)
         self.motor_right.run(-self.speed)
         duration = angle / (0.47 * self.speed)
-        wait(duration)
+        wait(duration * 1000)  # Convert to ms
         self.stop()
 
     def stop(self):
@@ -73,15 +64,13 @@ class Robot_Car:
         self.move_forward(distance)
         self.location = [x, y]
         self.orientation = atan2(dy, dx)
-    
 
 if __name__ == "__main__":
     car = Robot_Car()
-    ev3.speaker.beep()  
+    ev3.speaker.beep()
     car.move_to(0, 100)
     car.move_to(100, 100)
     car.move_to(100, 0)
     car.move_to(0, 0)
+    car.turn_left(3.14)
     ev3.speaker.beep()
-
-
