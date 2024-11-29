2a0592baa1a660f26527c9daef3fb6c98ad9bfac
Selina Lin <dev@lin.tf>
2024-11-29 17:06:07 +0800

    make RobotCommander a class

--- Server/RobotCommander.py
+++ Server/RobotCommander.py
@@ -1,20 +1,22 @@
 from pybricks.messaging import BluetoothMailboxClient, TextMailbox
 
-client = BluetoothMailboxClient()
-mbox = TextMailbox('talk', client)
+class RobotCommander:
+    def __init__(self):
+        self.client = BluetoothMailboxClient()
+        self.mbox = TextMailbox('talk', self.client)
 
-print("Connecting to EV3...")
-print("Connecting to EV3...")
-try:
-    client.connect('00:17:E9:F8:C1:77')
-    print("Connected successfully!")
-except OSError as e:
-    print(f"Connection failed: {e}")
-print("Connected!")
+        print("Connecting to EV3...")
+        try:
+            self.client.connect('00:17:E9:F8:C1:77')
+            print("Connected successfully!")
+        except OSError as e:
+            print(f"Connection failed: {e}")
+        print("Connected!")
 
-commands = ["FORWARD", "LEFT", "MOVE 100 100", "STOP"]
-for cmd in commands:
-    print(f"Sending: {cmd}")
-    mbox.send(cmd)
-    mbox.wait()  # Wait for EV3's confirmation
-    print("Response:", mbox.read())
+    def send_command(self, command):
+        # commands = ["FORWARD", "LEFT", "MOVE 100 100", "STOP"]
+        # for cmd in commands:
+        print(f"Sending: {cmd}")
+        self.mbox.send(cmd)
+        self.mbox.wait()  # Wait for EV3's confirmation
+        print("Response:", self.mbox.read())
